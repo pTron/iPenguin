ded4fb3dc279225ed2209c6569af021bb7e9eee2
Sinklier <iti7147@apl6904103.eagle.usaa.com>
2014-04-14 18:56:27 -0500

    first commit

---
+++ Hungry Penguins/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hungry Penguins
+//
+//  Created by Sinklier, Nicholas on 3/15/14.
+//  Copyright (c) 2014 Sinklier, Nicholas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hungry Penguins/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Hungry Penguins
+//
+
+//  Copyright (c) 2014 Sinklier, Nicholas. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
